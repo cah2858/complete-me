e1b770254eba37fc07cb25b7e912278385686f03
Casey Hoepner <caseyhoepner@gmail.com>
2018-08-28 15:51:32 -0600

    Finish insert functionality and alter test

--- test/Node-test.js
+++ test/Node-test.js
@@ -1,26 +1,22 @@
 import { expect } from 'chai';
-import Node from '../lib/Node';
+import Node from '../lib/node';
 
 describe('NODE', () => {
   let node;
 
   beforeEach(() => {
-    node = new Node('p');
-  });
+    node = new Node();
+  })
 
   it('should exist', () => {
     expect(node).to.exist;
-  }) 
-
-  it('should take a letter as an argument and assign the fist letter to the letter property of this obj', () => {
-    expect(node.letter).to.equal('p');
-  }) 
+  })
 
   it('should default end to false', () => {
-    expect(node.end).to.equal(false);
+    expect(node.end).to.equal(false)
   })
 
   it('should default children to an empty object', () => {
     expect(node.children).to.deep.equal({});
-  }) 
+  })
 })
--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,28 +1,40 @@
 import { expect } from 'chai';
 import Trie from '../lib/Trie';
+import Node from '../lib/Node';
 
 describe('TRIE', () => {
-  let prefixTrie;
+  let trie;
 
   beforeEach(() => {
-    prefixTrie = new Trie();
+    trie = new Trie();
   });
 
   it('should be a function', () => {
-    expect(prefixTrie).to.exist;
+    expect(trie).to.exist;
   });
 
   it('should start with a length of zero', () => {
-    expect(prefixTrie.totalWords).to.equal(0);
+    expect(trie.totalWords).to.eq(0);
   })
 
-  it('should default root to null', () => {
-    expect(prefixTrie.root).to.equal(null);
+  it('should default root to empty object', () => {
+    expect(trie.root.children).to.deep.eq({});
   })
 
   it('should increase totalWords each time we instantiate a new word', () => {
-    expect(prefixTrie.count()).to.equal(0);
-    prefixTrie.insert('poop')
-    expect(prefixTrie.count()).to.equal(1);
+    expect(trie.count()).to.eq(0);
+    trie.insert('poop')
+    expect(trie.count()).to.eq(1);
+  })
+
+  it('should insert word correctly when calling insert', () => {
+    trie.insert('race');
+    trie.insert('racecar');
+    trie.insert('hello');
+    trie.insert('help');
+
+    console.log(JSON.stringify(trie, null, 4))
+    expect(trie.root.letter).to.equal('h');
+    expect(trie.root.letter).to.eq('h');
   })
 });
