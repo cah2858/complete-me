7770457051c2fd8da2490b2e6fc9bfaec5ea942c
Casey Hoepner <caseyhoepner@gmail.com>
2018-09-05 19:49:50 -0600

    Import new complete me from other directory

--- test/Node-test.js
+++ test/Node-test.js
@@ -1,18 +1,32 @@
-import { expect } from 'chai';
-import Node from '../lib/node';
+const { expect } = require('chai')
+const Node = require('../lib/Node')
 
-describe('NODE', () => {
+describe('NODE', () =>  {
   let node;
 
   beforeEach(() => {
-    node = new Node();
-  })
+    node = new Node('p');
+  });
 
-  it('should exist', () => {
+  it('should exist', () =>  {
     expect(node).to.exist;
-  })
+  });
 
-  it('should default end to false', () => {
-    expect(node.end).to.equal(false)
+  it('should take a letter as an argument and assign it to the letter property', () => {
+    expect(node.letter).to.equal('p');
+  });
+
+  it('should default wordEnding to null', () => {
+    expect(node.wordEnding).to.equal(null);
+  });
+
+  it('should default children to an empty object', () => {
+    expect(node.children).to.deep.equal({});
+  });
+
+  it('should default selection count to 0', () => {
+    expect(node.selectionCount).to.equal(0)
   })
-})
+});
+
+
--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,13 +1,12 @@
-import { expect } from 'chai';
-import Trie from '../lib/Trie';
-import fs from 'fs';
-
+const { expect } = require('chai')
+const Trie = require('../lib/TrieList')
+const fs = require('fs');
+const text = "/usr/share/dict/words"
+const dictionary = fs.readFileSync(text).toString().trim().split('\n');
 
 describe('TRIE', () => {
   let trie;
-  const text = "/usr/share/dict/words";
-  const dictionary = fs.readFileSync(text).toString().trim().split('\n');
-  
+
   beforeEach(() => {
     trie = new Trie();
   });
@@ -16,41 +15,80 @@ describe('TRIE', () => {
     expect(trie).to.exist;
   })
 
-  it('should start with zero elements', () => {
-    expect(trie.totalWords).to.equal(0);
+  it('should default wordCount to 0', () => {
+    expect(trie.wordCount).to.equal(0);
   });
 
-  it('should increase totalWords each time we instantiate a new word', () => {
-    expect(trie.totalWords).to.eq(0);
-    trie.insert('hello')
-    expect(trie.totalWords).to.eq(1)
+  it('should default root to null', () => {
+    expect(trie.root).to.equal(null);
   });
 
-  it('should insert a word by invoking the insert method', () => {
-    trie.insert('hello');
-    trie.insert('cool');
-    trie.insert('puppy');
-    expect(trie.root).to.have.property('h') 
-    expect(trie.root).to.have.property('c') 
-    expect(trie.root).to.have.property('p') 
+  it('should increase the number of words in wordList each time we insert a new word', () => {
+    expect(trie.count()).to.equal(0);
+    trie.insert('word');
+    expect(trie.count()).to.equal(1);
   });
 
-  it('should pass in 2 words with one word having an additional letter', () => {
-    trie.insert('work');
-    trie.insert('works');
-    expect(trie.suggest('wor')).to.deep.eq(['work', 'works']);
+  it('should be able to take multiple words in wordList', () => {
+    trie.insert('word');
+    trie.insert('tacos');
+    trie.insert('a');
+    trie.insert('apple');
+    trie.insert('disestablishmentarianism');
+    expect(trie.count()).to.equal(5);
   });
 
-  it('should suggest an array of words', () => {
-    trie.insert('cat');
-    trie.insert('car');
-    let response = trie.suggest('ca');
-    // console.log(JSON.stringify(trie, null, 4))
-    expect(response).to.deep.eq(['cat', 'car'])
+  it('should create a new root if one does not exist', () => {
+    trie.insert('a');
+    expect(trie.root.children).to.deep.equal({a: {letter: 'a', selectionCount: 0, wordEnding: 'a', children: {}}});
   });
 
-  it('should populate with words', () => {
+  it('should set the wordEnding property of the last letter of a word to that word', () => {
+    trie.insert('do');
+    expect(trie.root.children.d.children.o.wordEnding).to.equal('do');
+  });
+
+  it('should have multiple nodes from words with the same starting letter that have a wordEnding', () => {
+    trie.insert('a');
+    trie.insert('ace');
+    expect(trie.root.children.a.wordEnding).to.equal('a');
+    expect(trie.root.children.a.children.c.children.e.wordEnding).to.equal('ace');
+  });
+
+  it('should return a suggestion if the entered word is a complete word', () => {
+    trie.insert('bat');
+    trie.insert('band');
+    trie.insert('batter');
+
+    expect(trie.suggest('ba')).to.deep.equal(['batter', 'bat', 'band']);
+  });
+
+  it('should not return a word as a suggestion if it does not match the prefix', () => {
+    trie.insert('frog');
+    trie.insert('toad');
+
+    expect(trie.suggest('f')).to.deep.equal(['frog']);
+  });
+  
+  it('should get words from the dictionary and put them into the trie', () => {
     trie.populate(dictionary);
-    expect(trie.totalWords).to.eq(235886);
+    expect(trie.count()).to.deep.equal(235886);
+  });
+
+  it('should take words from the dictionary and suggest them back with matching prefixes', () => {
+    trie.insert('help')
+    trie.insert('hello')
+    trie.insert('helicopter')
+    trie.insert('ham')
+    trie.insert('bye')
+
+    expect(trie.suggest('hel')).to.deep.equal(['help', 'hello', 'helicopter']);
+  });
+
+  it('should tell you if a word has no suggestions', () => {
+    trie.populate(dictionary);
+    expect(trie.suggest('sdlfkj')).to.deep.equal([]);
   });
 });
+
+
